bdeb302832c8367284137e305552edd9920983f7
Abhik Das <abhikdas98765@gmail.com>
2023-12-04 23:20:52 +0530

    Solved easy questions of 01-js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,38 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  
+  const occurrences1 = {}, occurrences2 = {};
 
+  for (let i = 0; i < str1.length; i++) {
+    const [l1, l2] = [str1[i].toLowerCase(), str2[i].toLowerCase()];
+
+    if (!occurrences1[l1]) {
+      occurrences1[l1] = 0;
+    }
+
+    if (!occurrences2[l2]) {
+      occurrences2[l2] = 0;
+    }
+    
+    occurrences1[l1]++;
+    occurrences2[l2]++;
+  }
+
+  if (Object.entries(occurrences1).length !== Object.entries(occurrences2).length) {
+    return false;
+  }
+
+  for (const letter in occurrences1) {
+    if (occurrences1[letter] !== occurrences2[letter]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = isAnagram;
--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -6,7 +6,26 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const spendingByCategory = {};
+
+  transactions.forEach((item) => {
+    if (!spendingByCategory[item.category]) {
+      spendingByCategory[item.category] = 0;
+    }
+
+    spendingByCategory[item.category] += parseInt(item.price);
+  })
+
+  const spendingByCategoryList = [];
+
+  for (const category in spendingByCategory) {
+    spendingByCategoryList.push({
+      category,
+      totalSpent: spendingByCategory[category]
+    });
+  }
+
+  return spendingByCategoryList;
 }
 
 module.exports = calculateTotalSpentByCategory;
